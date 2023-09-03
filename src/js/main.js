454dd4f8ed2fcea3bc8129ac8aeb1a0b5155770a
Drexlarr <diegodba29@gmail.com>
2023-09-03 01:00:29 -0500

    First Month

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,109 @@
 (function () {
-  const doc = document
-  const rootEl = doc.documentElement
-  const body = doc.body
-  const lightSwitch = doc.getElementById('lights-toggle')
+  const doc = document;
+  const rootEl = doc.documentElement;
+  const body = doc.body;
+  const lightSwitch = doc.getElementById("lights-toggle");
   /* global ScrollReveal */
-  const sr = window.sr = ScrollReveal()
+  const sr = (window.sr = ScrollReveal());
 
-  rootEl.classList.remove('no-js')
-  rootEl.classList.add('js')
+  rootEl.classList.remove("no-js");
+  rootEl.classList.add("js");
 
-  window.addEventListener('load', function () {
-    body.classList.add('is-loaded')
-  })
+  window.addEventListener("load", function () {
+    body.classList.add("is-loaded");
+  });
 
   // Reveal animations
-  function revealAnimations () {
-    sr.reveal('.feature', {
+  function revealAnimations() {
+    sr.reveal(".feature", {
       duration: 600,
-      distance: '20px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      origin: 'right',
-      viewFactor: 0.2
-    })
+      distance: "20px",
+      easing: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+      origin: "right",
+      viewFactor: 0.2,
+    });
   }
 
-  if (body.classList.contains('has-animations')) {
-    window.addEventListener('load', revealAnimations)
+  function updateCountdown() {
+    const now = new Date();
+    const targetDate = new Date(now.getFullYear(), now.getMonth(), 3);
+
+    // Si el 3 de este mes ya pasó, avanzamos al próximo mes
+    if (now.getDate() >= 3) {
+      targetDate.setMonth(targetDate.getMonth() + 1);
+    }
+
+    const timeRemaining = targetDate - now;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    const todayMessageElement = document.getElementById("todayMessage");
+    const countdownElement = document.getElementById("countdown");
+
+    if (now.getDate() === 3) {
+      todayMessageElement.textContent = "¡Es HOY!";
+    } else {
+      todayMessageElement.textContent = ""; // Borra el mensaje si no es el día 3
+
+      // Función para agregar un "0" adelante si el número es menor a 10
+    }
+
+    function addLeadingZero(num) {
+      return num < 10 ? "0" + num : num;
+    }
+
+    countdownElement.innerHTML = `${addLeadingZero(days)}:${addLeadingZero(
+      hours
+    )}:${addLeadingZero(minutes)}:${addLeadingZero(seconds)}`;
+  }
+
+  // Actualizar cada segundo
+  setInterval(updateCountdown, 1000);
+
+  // Actualizar al cargar la página
+  updateCountdown();
+
+  // Actualizar cada segundo
+  setInterval(updateCountdown, 1000);
+
+  // Actualizar al cargar la página
+  updateCountdown();
+
+  // Actualizar cada segundo
+  setInterval(updateCountdown, 1000);
+
+  // Actualizar al cargar la página
+  updateCountdown();
+
+  if (body.classList.contains("has-animations")) {
+    window.addEventListener("load", revealAnimations);
   }
 
   // Light switcher
   if (lightSwitch) {
-    window.addEventListener('load', checkLights)
-    lightSwitch.addEventListener('change', checkLights)
+    window.addEventListener("load", checkLights);
+    lightSwitch.addEventListener("change", checkLights);
   }
 
-  function checkLights () {
-    let labelText = lightSwitch.parentNode.querySelector('.label-text')
+  function checkLights() {
+    let labelText = lightSwitch.parentNode.querySelector(".label-text");
     if (lightSwitch.checked) {
-      body.classList.remove('lights-off')
+      body.classList.remove("lights-off");
       if (labelText) {
-        labelText.innerHTML = 'dark'
+        labelText.innerHTML = "dark";
       }
     } else {
-      body.classList.add('lights-off')
+      body.classList.add("lights-off");
       if (labelText) {
-        labelText.innerHTML = 'light'
+        labelText.innerHTML = "light";
       }
     }
   }
-}())
+})();
