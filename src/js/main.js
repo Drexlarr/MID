ace1cf1b1f1c9e15b2fc8c1480ba71fe8f350133
Diego Bazan <58017210+Drexlarr@users.noreply.github.com>
2024-02-28 11:01:22 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 let clickCount = 0;
 
 window.addEventListener("load", function () {
-  window.opener.postMessage("Ready", "*");
+    const request = {
+        eventName: "on-core-portal-is-ready",
+    };
+  window.opener.postMessage(JSON.stringify(request), "*");
 
   window.addEventListener("message", function (event) {
     console.log("Nuevo mensaje");
@@ -9,5 +12,8 @@ window.addEventListener("load", function () {
 });
 
 window.addEventListener("beforeunload", function () {
-  window.opener.postMessage("ConnectionLost", "*");
+    const request = {
+        eventName: "on-core-portal-is-closed",
+    };
+  window.opener.postMessage(JSON.stringify(request), "*");
 });
